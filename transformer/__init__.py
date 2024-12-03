9beacfd9266ad3048ed6bf44ef9f6c3b885f68b6
Tejas Prabhune <prabhune@berkeley.edu>
2024-12-02 19:58:27 -0800

    Add training script

--- transformer/__init__.py
+++ transformer/__init__.py
@@ -2,3 +2,4 @@ from .encoder import Encoder, EncoderLayer, PositionalEncoding
 from .decoder import Decoder, DecoderLayer
 from .attention import MultiHeadAttention, FeedForwardNN
 from .transformer import Transformer
+from .data import FrenchEnglishDataset
